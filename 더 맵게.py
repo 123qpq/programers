b14043374daf475138d29f3dba95c6da629d2abe
123qpq <45002168+123qpq@users.noreply.github.com>
2020-12-29 00:45:12 +0900

    Update 더 맵게.py
    second try : success! learn about heapq and is more faster

--- 더 맵게.py
+++ 더 맵게.py
@@ -1,13 +1,15 @@
+import heapq
 def solution(scoville, K):
     answer = 0
-    scoville = sorted(scoville, reverse=True)
-    while scoville[-1] < K:
+    heapq.heapify(scoville)
+
+    while scoville[0] < K:
         if len(scoville) < 2:
             return -1
-        min1 = scoville.pop()
-        min2 = scoville.pop()
-        scoville.append(min1 + 2 * min2)
-        scoville = sorted(scoville, reverse=True)
+        min1 = heapq.heappop(scoville)
+        min2 = heapq.heappop(scoville)
+        x = min1 + 2 * min2
+        heapq.heappush(scoville, x)
         answer += 1
-
+    
     return answer
