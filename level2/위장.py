146cd9ebb5e03fc215da971c0dcce4f4141bb317
123qpq <45002168+123qpq@users.noreply.github.com>
2020-12-31 12:51:30 +0900

    Update 위장.py
    second try : success! think about number of cases!

--- level2/위장.py
+++ level2/위장.py
@@ -1,20 +1,13 @@
 def solution(clothes):
-    answer = 0
+    answer = 1
     dic = {}
     for clothe in clothes:
         if clothe[1] not in dic:
-            dic[clothe[1]] = 1
+            dic[clothe[1]] = 2
         else:
             dic[clothe[1]] += 1
     
-    size = len(dic)
-    dic = list(dic.values())
-
-    for i in range(1, 2**size):
-        mul = 1
-        flag = bin(i)[2:].zfill(size)
-        for j in range(size):
-            if flag[j] == '1':
-                mul *= dic[j]
-        answer += mul
-    return answer
+    for i in list(dic.values()):
+        answer *= i
+        
+    return answer - 1
