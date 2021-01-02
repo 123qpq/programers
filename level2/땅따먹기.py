4feeb3f6fb44b61c3e61e8e796bf71ff87d79240
123qpq <45002168+123qpq@users.noreply.github.com>
2021-01-02 22:47:18 +0900

    Update 땅따먹기.py
    second try : have to do study..

--- level2/땅따먹기.py
+++ level2/땅따먹기.py
@@ -1,19 +1,5 @@
 def solution(land):
-    answer = 0
-    land.append([0, 0, 0, 0])
-    for i in range(len(land)-1):
-        nowmax = max(land[i])
-        nextmax = max(land[i+1])
-        if land[i].index(nowmax) == land[i+1].index(nextmax):
-            if sorted(land[i])[-1] + sorted(land[i+1])[-2] < sorted(land[i])[-2] + sorted(land[i+1])[-1]:
-                print(answer, sorted(land[i])[-2])
-                answer += sorted(land[i])[-2]
-            else:
-                print(answer, nowmax)
-                answer += nowmax + sorted(land[i+1])[-2] - sorted(land[i+1])[-1]
-        else:
-            print(answer, nowmax)
-            answer += nowmax
-            
-
-    return answer
+    for i in range(1, len(land)):
+        for j in range(4):
+            land[i][j] += max(land[i-1][:j] + land[i-1][j+1:])
+    return max(land[-1])
