6a0ee9a319e882b205e9293f361fcb5e27dc18ae
123qpq <45002168+123qpq@users.noreply.github.com>
2021-01-26 22:11:33 +0900

    Update N으로 표현.py
    fail. but better than before.

--- level3/N으로 표현.py
+++ level3/N으로 표현.py
@@ -1,12 +1,30 @@
 def solution(N, number):
-    if N == 1:
-        return number if number <= 8 else -1
-    lst = [0]
-    for i in range(1, N):
-        lst.append(i+1)
-    lst.append(1)
-    print(lst)
-    print(number//N, number%N)
-    
-    answer = 0
-    return answer
+    a = []
+    lst = [[11, 11, 1, 1, 1, 1], [11, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1]]
+    '''
+    if number == N:
+        return 1
+    for i in range(10922, 255, -1):
+        temp = list(filter(None, (bin(i)[2:] + '1').split('0')))
+        if temp not in lst and ''.join(temp).count('1') == 8:
+            lst.append(temp)
+            #print(temp)
+    '''
+    for i in lst:
+        search = [N]
+        answer = 1
+        for j in i:
+            answer += len(str(j))
+            add = list(map(lambda x : x + j*N, search))
+            div = list(map(lambda x : x // (j*N), search))
+            mul = list(map(lambda x : x * j*N, search))
+            new = list(set(add + div + mul))
+            if number in new:
+                a.append(answer)
+            else:
+                search = new
+    print(a)
+    if len(a) != 0:
+        return min(a)
+    else:
+        return -1
