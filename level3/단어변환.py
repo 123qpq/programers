5b08de5e8e95bfcfa990a06e182824ca259c22e6
123qpq <45002168+123qpq@users.noreply.github.com>
2021-01-28 12:28:54 +0900

    Update 단어변환.py
    60 point, use dfs(depth first search)

--- level3/단어변환.py
+++ level3/단어변환.py
@@ -1,15 +1,29 @@
 def solution(begin, target, words):
     if target not in words:
         return 0
-    comp = begin
-    graph = []
+    graph = {}
     
+    words.append(begin)
     for word in words:
         temp = []
-        cnt = len([x for x in begin if x in word])
-        if cnt == 2:
-            temp.append(word)
-            words.remove(word)
-        graph.append(temp)
-                
+        for j in words:
+            cnt = len([x for x in word if x in j])
+            if cnt == len(begin) - 1: #한글자만 다른 경우 모음
+                temp.append(j)
+        graph[word] = set(temp) # 그래프 구축
+    print(graph)
+    
+    visited = []
+    stack = [begin]
+    
+    while stack: #깊이탐색
+        n = stack.pop()
+        if n not in visited:
+            visited.append(n)
+            stack += graph[n] - set(visited)
+            print(stack, visited)
+        if target in stack:
+            print(visited)
+            return len(visited)
+
     return 0
