8033b13fd845f2939fa1cb5fbbf80fb9fef42fee
123qpq <45002168+123qpq@users.noreply.github.com>
2021-02-12 23:18:11 +0900

    Update 섬 연결하기.py
    success. use prime algorithm

--- level3/섬 연결하기.py
+++ level3/섬 연결하기.py
@@ -1,36 +1,26 @@
+import heapq
 def solution(n, costs):
     
-    table = [[99 for i in range(n)] for j in range(n)]
-    p = 1
-    answer = 0
-    lands = [0]
+    total_cost = 0
+    nodes = {i:[] for i in range(n)}
     
-    for x, y, z in costs: #인접행렬 생성
-            table[x][y] = z
-            table[y][x] = z
+    for x, y, z in costs:
+        nodes[x].append((z, y))
+        nodes[y].append((z, x))
     
-    for t in table:
-        print(t)
-    print()
-    #while len(lands) != n:
-    for i in range(n):
-        temp = [99 for i in range(n)]
-        for land in lands:
-            m = min(table[land])
-            temp[table[land].index(m)] = min(m, temp[table[land].index(m)])#정복한 땅에서 갈 수 있는 최소값 잡아냄
-        #과거에 있던 값이 덮어씌워지는 오류가 있음. 최저값이 제대로 안잡힌다.
-        print(temp)
-        x = temp.index(min(temp))
-        print(land, x, m, lands)
-        if x not in lands:
-            lands.append(x)
-            table[land][x] = 99
-            table[x][land] = 99
-            answer += m
-        if len(lands) == n:
-            break
-
-    print(lands)
-
-
-    return answer
+    connected = [0]
+    candidate = nodes[0]
+    heapq.heapify(candidate)
+    
+    while candidate:
+        cost, y = heapq.heappop(candidate)
+        
+        if y not in connected: #사이클이 생기지 않으면
+            connected.append(y) #새 노드 연결
+            total_cost += cost #연결값 반영
+            
+            for n in nodes[y]: #다음노드에서 이어갈 수 있는 노드들
+                if n[1] not in connected: #만약 그 다음노드들이 사이클을 만들지 않으면
+                    heapq.heappush(candidate, n) #힙에 추가한다
+    
+    return total_cost
