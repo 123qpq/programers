c1cad612c1f382f92be1de7ca34bcddb6b1343e2
123qpq <45002168+123qpq@users.noreply.github.com>
2021-02-13 09:53:08 +0900

    Update 체육복.py
    ra?? success. just changed (for)s.  i don't know why it success

--- level1/체육복.py
+++ level1/체육복.py
@@ -7,13 +7,14 @@ def solution(n, lost, reserve):
         if i+1 in reserve:
             student[i] += 1
             
-    for i in range(n-1):
-        if student[i] == 2 and student[i+1] == 0:
-            student[i] = 1
-            student[i+1] = 1
     for i in range(1,n):
         if student[i] == 2 and student[i-1] == 0:
             student[i] = 1
             student[i-1] = 1
+    for i in range(n-1):
+        if student[i] == 2 and student[i+1] == 0:
+            student[i] = 1
+            student[i+1] = 1
+
     
     return student.count(1) + student.count(2)
